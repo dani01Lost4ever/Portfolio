f3a4fdcaa200d33d725b4c92faf2ad6daa6932fe
Daniel <daniel.busetto.db@gmail.com>
2023-07-14 21:44:08 +0200

    prime modifiche

--- app.js
+++ app.js
@@ -2,23 +2,23 @@ particlesJS('particles-js',
 {
   "particles": {
     "number": {
-      "value": 160,
+      "value": 120,
       "density": {
         "enable": true,
-        "value_area": 4000
+        "value_area": 1000
       }
     },
     "color": {
-      "value": "#ff3232"
+      "value": "#ffffff"
     },
     "shape": {
       "type": "circle",
       "stroke": {
         "width": 0,
-        "color": "#ff3232"
+        "color": "#000000"
       },
       "polygon": {
-        "nb_sides": 5
+        "nb_sides": 12
       },
       "image": {
         "src": "img/github.svg",
@@ -27,27 +27,27 @@ particlesJS('particles-js',
       }
     },
     "opacity": {
-      "value": 1,
-      "random": true,
+      "value": 0.7,
+      "random": false,
       "anim": {
-        "enable": true,
-        "speed": 0.5,
-        "opacity_min": 0,
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
         "sync": false
       }
     },
     "size": {
-      "value": 5,
+      "value": 3,
       "random": true,
       "anim": {
         "enable": false,
-        "speed": 2,
-        "size_min": 0.3,
+        "speed": 40,
+        "size_min": 0.1,
         "sync": false
       }
     },
     "line_linked": {
-      "enable": false,
+      "enable": true,
       "distance": 150,
       "color": "#ffffff",
       "opacity": 0.4,
@@ -55,7 +55,7 @@ particlesJS('particles-js',
     },
     "move": {
       "enable": true,
-      "speed": 1,
+      "speed": 6,
       "direction": "none",
       "random": true,
       "straight": false,
@@ -64,7 +64,7 @@ particlesJS('particles-js',
       "attract": {
         "enable": false,
         "rotateX": 600,
-        "rotateY": 600
+        "rotateY": 1200
       }
     }
   },
@@ -73,11 +73,11 @@ particlesJS('particles-js',
     "events": {
       "onhover": {
         "enable": false,
-        "mode": "bubble"
+        "mode": "repulse"
       },
       "onclick": {
-        "enable": false,
-        "mode": "repulse"
+        "enable": true,
+        "mode": "push"
       },
       "resize": true
     },
@@ -89,14 +89,14 @@ particlesJS('particles-js',
         }
       },
       "bubble": {
-        "distance": 250,
-        "size": 0,
+        "distance": 400,
+        "size": 40,
         "duration": 2,
-        "opacity": 0,
+        "opacity": 8,
         "speed": 3
       },
       "repulse": {
-        "distance": 400,
+        "distance": 200,
         "duration": 0.4
       },
       "push": {
